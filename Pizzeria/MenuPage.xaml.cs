aafefe925fb5f65993bea1c4ba0344739ac3b2b8
4TP1 <4TP1@DESKTOP-QJ1MR0J>
2026-05-21 14:41:27 +0200

    Update

--- Pizzeria/MenuPage.xaml.cs
+++ Pizzeria/MenuPage.xaml.cs
@@ -1,19 +1,44 @@
-﻿using System.Collections.ObjectModel;
+﻿using Android.OS;
+using AndroidX.Core.Provider;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace Pizzeria
 {
-    public class Danie
+    public class Danie : ContentPage
     {
         public string Nazwa { get; set; }
         public string Opis { get; set; }
-        public decimal Cena { get; set; }
+        public double Cena { get; set; }
         public string Obraz { get; set; }
-        public Danie(string nazwa, string opis, decimal cena, string obraz)
+        public bool Wysuniete
+        {
+            get;
+            set {
+                if(Wysuniete != value)
+                {
+                    Wysuniete = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public event PropertyChangingEventHandler? PropertyChanged;
+        protected override void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(
+                this,
+                new
+                PropertyChangingEventArgs(propertyName)
+            );
+        }
+        public Danie(string nazwa, string opis, double cena, string obraz)
         {
             Nazwa = nazwa;
             Opis = opis;
             Cena = cena;
             Obraz = obraz;
+            Wysuniete = false;
         }
     }
 
@@ -21,20 +46,29 @@ namespace Pizzeria
     {
         public ObservableCollection<Danie> Menu { get; } = new ObservableCollection<Danie>
         {
-            new Danie("Pizza", "Pizzowa", 12_50, "pizza_image.png"),
-            new Danie("Pizza2", "Pizzogranie", 25_39, "pizza_image.png")
+            new Danie("Pizza", "Pizzowa", 12.50, "pizza_image.png"),
+            new Danie("Pizza2", "Pizzogranie", 25.39, "pizza_image.png")
         };
 
         public MenuPage()
         {
             InitializeComponent();
+
+            CultureInfo culture = new CultureInfo("pl-PL");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
             BindingContext = this;
             CollectionMenu.ItemsSource = Menu;
         }
 
         private void MenuTab_Tapped(object sender, TappedEventArgs e)
         {
-            
+            HorizontalStackLayout stack = sender as HorizontalStackLayout;
+            Danie danie = stack.BindingContext as Danie;
+
+            //DisplayAlert("a", danie.Nazwa, "a");
+            danie.Wysuniete = !danie.Wysuniete;
         }
     }
 }
